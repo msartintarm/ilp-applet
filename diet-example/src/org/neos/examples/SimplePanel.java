f6fd7ac1dddb7c8768dc76b775efc5e9a5913a51
Michael Sartin-Tarm <msartintarm@gmail.com>
2013-06-01 18:05:21 -0500

    discovered weird issue where an outdated class is created and used. working on fixing

--- diet-example/src/org/neos/examples/SimplePanel.java
+++ diet-example/src/org/neos/examples/SimplePanel.java
@@ -29,113 +29,113 @@ import org.neos.client.ResultReceiver;
  *
  */
 public class SimplePanel extends JPanel implements ActionListener,
-		ResultCallback {
+						   ResultCallback {
 
-	private static final String SUBMIT_COMMAND = "submit";
-	private static final String CLEAR_COMMAND = "clear";
+    private static final String SUBMIT_COMMAND = "submit";
+    private static final String CLEAR_COMMAND = "clear";
 
-	private static final String HOST = "www.neos-server.org";
-	private static final String PORT = "3332";
+    private static final String HOST = "neos1.chtc.wisc.edu";
+    private static final String PORT = "3332";
 
-	private JTextArea outputTextArea;
-	private JButton submitButton;
-	private JButton clearButton;
-	private NeosXmlRpcClient client;
-	private ResultReceiver reciever;
+    private JTextArea outputTextArea;
+    private JButton submitButton;
+    private JButton clearButton;
+    private NeosXmlRpcClient client;
+    private ResultReceiver reciever;
 
-	public SimplePanel() {
-		super(new BorderLayout());
+    public SimplePanel() {
+	super(new BorderLayout());
 
-		JButton submitButton = new JButton("Submit");
-		submitButton.setActionCommand(SUBMIT_COMMAND);
-		submitButton.addActionListener(this);
+	JButton submitButton = new JButton("Submit");
+	submitButton.setActionCommand(SUBMIT_COMMAND);
+	submitButton.addActionListener(this);
 
-		JButton clearButton = new JButton("Clear");
-		clearButton.setActionCommand(CLEAR_COMMAND);
-		clearButton.addActionListener(this);
+	JButton clearButton = new JButton("Clear");
+	clearButton.setActionCommand(CLEAR_COMMAND);
+	clearButton.addActionListener(this);
 
-		outputTextArea = new JTextArea(20, 20);
-		outputTextArea.setEditable(false);
-		outputTextArea.setText("init");
-		// Lay everything out.
+	outputTextArea = new JTextArea(20, 20);
+	outputTextArea.setEditable(false);
+	outputTextArea.setText("initz");
+	// Lay everything out.
 
-		JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
-		// buttonPanel.setPreferredSize(new Dimension(200, 30));
-		buttonPanel.add(submitButton);
-		buttonPanel.add(clearButton);
-		add(buttonPanel, BorderLayout.NORTH);
+	JPanel buttonPanel = new JPanel(new GridLayout(1, 2));
+	// buttonPanel.setPreferredSize(new Dimension(200, 30));
+	buttonPanel.add(submitButton);
+	buttonPanel.add(clearButton);
+	add(buttonPanel, BorderLayout.NORTH);
 
-		JPanel outputPanel = new JPanel(new FlowLayout());
-		// outputPanel.setPreferredSize(new Dimension(200, 120));
-		outputPanel.add(outputTextArea);
-		add(outputPanel, BorderLayout.CENTER);
+	JPanel outputPanel = new JPanel(new FlowLayout());
+	// outputPanel.setPreferredSize(new Dimension(200, 120));
+	outputPanel.add(outputTextArea);
+	add(outputPanel, BorderLayout.CENTER);
 
-		setSize(new Dimension(300, 200));
+	setSize(new Dimension(300, 200));
 
-		client = new NeosXmlRpcClient(HOST, PORT);
+	client = new NeosXmlRpcClient(HOST, PORT);
 
-		try {
-			client.connect();
-		} catch (XmlRpcException e) {
-			outputTextArea.setText("Unable to connect to NEOS server :"
-					+ e.getMessage());
-		}
+	try {
+	    client.connect();
+	} catch (XmlRpcException e) {
+	    outputTextArea.setText("Unable to connect to NEOS server :"
+				   + e.getMessage());
 	}
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		String command = e.getActionCommand();
+    public void actionPerformed(ActionEvent e) {
+	String command = e.getActionCommand();
 
-		if (command.equals(SUBMIT_COMMAND)) {
-			submitJob();
+	if (command.equals(SUBMIT_COMMAND)) {
+	    submitJob();
 
-		} else if (command.equals(CLEAR_COMMAND)) {
-			outputTextArea.setText("");
-			System.out.println("clear");
-		}
+	} else if (command.equals(CLEAR_COMMAND)) {
+	    outputTextArea.setText("");
+	    System.out.println("clear");
 	}
+    }
 
-	public void submitJob() {
-		NeosJobXml job = new NeosJobXml("test", "HelloNEOS", "default");
-		job.addParam("num1", "55");
-		job.addParam("num2", "100");
-		job.addParam("operation", "Multiplication");
+    public void submitJob() {
+	NeosJobXml job = new NeosJobXml("test", "HelloNEOS", "default");
+	job.addParam("num1", "55");
+	job.addParam("num2", "100");
+	job.addParam("operation", "Multiplication");
 
-		Vector params = new Vector();
-		// Job xml
-		params.add(job.toXMLString());
+	Vector params = new Vector();
+	// Job xml
+	params.add(job.toXMLString());
 		
-		Integer currentJob = 0;
-		String currentPassword = "";
+	Integer currentJob = 0;
+	String currentPassword = "";
 
-		try {
-			Object[] results = (Object[]) client.execute("submitJob", params);
+	try {
+	    Object[] results = (Object[]) client.execute("submitJob", params);
 
-			currentJob = (Integer) results[0];
-			currentPassword = (String) results[1];
-		} catch (XmlRpcException e) {
-			outputTextArea.setText("Error submitting job :" + e.getMessage());
-			return;
-		}
-		if (currentJob == 0) {
-			System.out.println(currentPassword);
-			return;
-		}
-		reciever = new ResultReceiver(client, this, currentJob, currentPassword);
-		reciever.start();
-
+	    currentJob = (Integer) results[0];
+	    currentPassword = (String) results[1];
+	} catch (XmlRpcException e) {
+	    outputTextArea.setText("Error submitting job :" + e.getMessage());
+	    return;
 	}
+	if (currentJob == 0) {
+	    System.out.println(currentPassword);
+	    return;
+	}
+	reciever = new ResultReceiver(client, this, currentJob, currentPassword);
+	reciever.start();
 
-	@Override
-	public void handleJobInfo(int jobNo, String pass) {
-		// TODO Auto-generated method stub
+    }
+
+    @Override
+    public void handleJobInfo(int jobNo, String pass) {
+	// TODO Auto-generated method stub
 		
-	}
+    }
 	
-	@Override
-	public void handleFinalResult(String results) {
-		outputTextArea.setText(results);
+    @Override
+    public void handleFinalResult(String results) {
+	outputTextArea.setText(results);
 
-	}
+    }
 
 	
 
