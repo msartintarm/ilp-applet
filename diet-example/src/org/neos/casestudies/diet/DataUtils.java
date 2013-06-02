d9d35f9b26786c53930399cd097bbe5b5977906d
Michael Sartin-Tarm <msartintarm@gmail.com>
2013-06-02 17:05:32 -0500

    whitespace change

--- diet-example/src/org/neos/casestudies/diet/DataUtils.java
+++ diet-example/src/org/neos/casestudies/diet/DataUtils.java
@@ -20,173 +20,173 @@ import org.neos.casestudies.diet.Nutrient;
  */
 public class DataUtils {
 
-	/**
-	 * Input data for nutrients
-	 */
-	public static final String NUTRIENTS = "resources/nutrients.conf";
+    /**
+     * Input data for nutrients
+     */
+    public static final String NUTRIENTS = "resources/nutrients.conf";
 
-	/**
-	 * Input data for food
-	 */
-	public static final String FOOD = "resources/food.conf";
+    /**
+     * Input data for food
+     */
+    public static final String FOOD = "resources/food.conf";
 
-	/**
-	 * Static part of the model
-	 */
-	public static final String MODEL = "resources/dietgams.mod";
+    /**
+     * Static part of the model
+     */
+    public static final String MODEL = "resources/dietgams.mod";
 
-	/**
-	 * Example output for feasible problem, for testing
-	 */
-	public static final String FEASIBLE = "resources/feasible.txt";
+    /**
+     * Example output for feasible problem, for testing
+     */
+    public static final String FEASIBLE = "resources/feasible.txt";
 
-	/**
-	 * Example output for infeasible problem, for testing
-	 */
-	public static final String INFEASIBLE = "resources/infeasible.txt";
+    /**
+     * Example output for infeasible problem, for testing
+     */
+    public static final String INFEASIBLE = "resources/infeasible.txt";
 
-	public static final int APPLICATION_MODE = 0;
-	public static final int APPLET_MODE = 1;
+    public static final int APPLICATION_MODE = 0;
+    public static final int APPLET_MODE = 1;
 
-	private static DataUtils self = null;
+    private static DataUtils self = null;
 
-	private int mode;
+    private int mode;
 
-	private DataUtils() {
+    private DataUtils() {
 
+    }
+
+    public static DataUtils getInstance(int mode) {
+	if (self == null) {
+	    self = new DataUtils();
+	    self.mode = mode;
 	}
 
-	public static DataUtils getInstance(int mode) {
-		if (self == null) {
-			self = new DataUtils();
-			self.mode = mode;
+	return self;
+    }
+
+    public List<Food> readFood(List<Nutrient> nutrients) {
+	LinkedList<Food> foodList = new LinkedList<Food>();
+
+	String line;
+	BufferedReader in;
+	try {
+	    Reader reader;
+	    if (mode == APPLET_MODE)
+		reader = new InputStreamReader(
+					       DataUtils.class.getResourceAsStream("/" + FOOD));
+	    else
+		reader = new FileReader(FOOD);
+
+	    in = new BufferedReader(reader);
+
+	    while ((line = in.readLine()) != null) {
+
+		line = line.trim();
+		if (line.length() == 0)
+		    continue;
+
+		String[] cols = line.split(";");
+
+		String vals = cols[2].trim();
+		String[] values = vals.split("\\s+");
+
+		Food food = new Food();
+
+		food.setLongName(cols[0].trim());
+		food.setServingSize(cols[1].trim());
+		food.setShortName(values[0]);
+		food.setCost(values[1]);
+
+		int i = 2;
+		for (Nutrient nut : nutrients) {
+		    food.addNutrient(nut.getName(), values[i++]);
 		}
-
-		return self;
+		foodList.add(food);
+	    }
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	public List<Food> readFood(List<Nutrient> nutrients) {
-		LinkedList<Food> foodList = new LinkedList<Food>();
+	return foodList;
+    }
 
-		String line;
-		BufferedReader in;
-		try {
-			Reader reader;
-			if (mode == APPLET_MODE)
-				reader = new InputStreamReader(
-						DataUtils.class.getResourceAsStream("/" + FOOD));
-			else
-				reader = new FileReader(FOOD);
+    public List<Nutrient> readNutrient() {
+	LinkedList<Nutrient> nutList = new LinkedList<Nutrient>();
 
-			in = new BufferedReader(reader);
+	String line;
+	BufferedReader in;
+	try {
 
-			while ((line = in.readLine()) != null) {
+	    Reader reader;
+	    if (mode == APPLET_MODE)
+		reader = new InputStreamReader(
+					       DataUtils.class.getResourceAsStream("/" + NUTRIENTS));
+	    else
+		reader = new FileReader(NUTRIENTS);
 
-				line = line.trim();
-				if (line.length() == 0)
-					continue;
+	    in = new BufferedReader(reader);
 
-				String[] cols = line.split(";");
+	    while ((line = in.readLine()) != null) {
 
-				String vals = cols[2].trim();
-				String[] values = vals.split("\\s+");
+		line = line.trim();
+		if (line.length() == 0)
+		    continue;
 
-				Food food = new Food();
+		String[] cols = line.split("\\s+");
 
-				food.setLongName(cols[0].trim());
-				food.setServingSize(cols[1].trim());
-				food.setShortName(values[0]);
-				food.setCost(values[1]);
+		Nutrient nut = new Nutrient();
 
-				int i = 2;
-				for (Nutrient nut : nutrients) {
-					food.addNutrient(nut.getName(), values[i++]);
-				}
-				foodList.add(food);
-			}
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		nut.setName(cols[0]);
+		nut.setMin(cols[1]);
+		nut.setMax(cols[2]);
+		nut.setUnit(cols[3]);
 
-		return foodList;
+		nutList.add(nut);
+	    }
+
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	public List<Nutrient> readNutrient() {
-		LinkedList<Nutrient> nutList = new LinkedList<Nutrient>();
+	return nutList;
+    }
 
-		String line;
-		BufferedReader in;
-		try {
+    public String readFile(String fileName) {
 
-			Reader reader;
-			if (mode == APPLET_MODE)
-				reader = new InputStreamReader(
-						DataUtils.class.getResourceAsStream("/" + NUTRIENTS));
-			else
-				reader = new FileReader(NUTRIENTS);
+	StringBuffer content = new StringBuffer();
+	String line;
+	BufferedReader in;
+	try {
+	    Reader reader;
+	    if (mode == APPLET_MODE)
+		reader = new InputStreamReader(
+					       DataUtils.class.getResourceAsStream("/" + fileName));
+	    else
+		reader = new FileReader(fileName);
 
-			in = new BufferedReader(reader);
+	    in = new BufferedReader(reader);
 
-			while ((line = in.readLine()) != null) {
+	    while ((line = in.readLine()) != null)
+		content.append(line + "\n");
 
-				line = line.trim();
-				if (line.length() == 0)
-					continue;
-
-				String[] cols = line.split("\\s+");
-
-				Nutrient nut = new Nutrient();
-
-				nut.setName(cols[0]);
-				nut.setMin(cols[1]);
-				nut.setMax(cols[2]);
-				nut.setUnit(cols[3]);
-
-				nutList.add(nut);
-			}
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return nutList;
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-	public String readFile(String fileName) {
-
-		StringBuffer content = new StringBuffer();
-		String line;
-		BufferedReader in;
-		try {
-			Reader reader;
-			if (mode == APPLET_MODE)
-				reader = new InputStreamReader(
-						DataUtils.class.getResourceAsStream("/" + fileName));
-			else
-				reader = new FileReader(fileName);
-
-			in = new BufferedReader(reader);
-
-			while ((line = in.readLine()) != null)
-				content.append(line + "\n");
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return content.toString();
-	}
+	return content.toString();
+    }
 
 }
