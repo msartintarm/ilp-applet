9aa6b54c6efb33ec9a9b210959bbbc2253e1ce94
Michael Sartin-Tarm <msartintarm@gmail.com>
2013-07-06 23:33:26 -0500

    working version of file up. test in 'javascript/example.html'

--- diet-example/signjar.sh
+++ diet-example/signjar.sh
@@ -1,1 +1,7 @@
-jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed -signedjar SignedDiet.jar Diet.jar jarkey
+
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/Neos.jar jarkey
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/slib/commons-logging-1.1.jar jarkey
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/slib/ws-commons-util-1.0.2.jar jarkey
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/slib/xmlrpc-common-3.1.3.jar jarkey
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/slib/xmlrpc-client-3.1.3.jar jarkey
+jarsigner -keystore key/keystore -storepass qawsed -keypass qawsed ../eclipse/Neos.jar jarkey
